5cb666c4b85a8eaeca14e0b00a6dcb7a42df0c26
amit99911 <104908447+amit99911@users.noreply.github.com>
2026-01-06 09:20:08 +0530

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,9 @@
+# Configuration and constants used in the script. Update values as needed.
+
+USERNAME = "amit99911@live.com"
+PASSWORD = "Reset@123"
+MOBILE = "7795227944"
+ORIGINAL_RESUME_PATH="C:/Users/amit.sinha.ext_littl/Downloads/Amit Sinha_Program Manager_12 Years_NASDAQ.pdf"
+MODIFIED_RESUME_PATH="C:/Users/amit.sinha.ext_littl/Downloads/Amit Sinha_Program Manager_12 Years_NASDAQ.pdf"
+NAUKRI_LOGIN_URL="https://www.naukri.com/nlogin/login"
+NAUKRI_PROFILE_URL="https://www.naukri.com/mnjuser/profile"
